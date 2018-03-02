326ebe4c084c3cd88945fe10d7b4ec50e328a8b5
Killy85 <goureau.n.a@hotmail.fr>
2018-03-02 10:39:40 +0100

    Exercice 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,8 +1,22 @@
 const moment = require("moment");
 
 const sortProduct = input => {
-  // Your future job begins here ...
+   let res = input.map(value => {
+      var date = new Date(value.dateAdded)
+      value.monthOfYear = date.getMonth()+1
+      value.year = date.getFullYear()
+      delete value.dateAdded
+      return value
+    })
+    res =res.sort((a,b) => a.quantity < b.quantity).sort((a,b) => a.year < b.year).sort((a,b) => a.monthOfYear < b.monthOfYear).filter(value => value.year > 2000)
+    res = res.reduce(function(acc, x) {
+      (acc[x.name] = acc[x.name] || []).push(x);
+      return acc;
+    }, {})
+    console.log(res)
+    return(res)
 };
+
 
 module.exports = {
   title: "Exercise 1",
