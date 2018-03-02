021cc61bb9df141d50f50055fee3cfcff8e02d3c
Killy85 <goureau.n.a@hotmail.fr>
2018-03-02 10:30:42 +0100

    Exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,6 +1,19 @@
 const countSequence = (sequence, input) => {
-  // Your future job begins here ...
-};
+    var length = sequence.length
+    if(input != undefined){
+      if(input.length<=0) return 0
+      let res = input.slice(0,length) === sequence ? 1 : 0
+      let sliceDist = res ===1 ? length : 1
+      return (res  + countSequence(sequence, input.slice(sliceDist)))
+    } else {
+      return function(x) {
+        if(x.length<=0) return 0
+        let res = x.slice(0,length) === sequence ? 1 : 0
+        let sliceDist = res ===1 ? length : 1
+        return res + countSequence(sequence, x.slice(sliceDist))
+      }
+    }
+  }
 
 module.exports = {
   title: "Exercise 4",
